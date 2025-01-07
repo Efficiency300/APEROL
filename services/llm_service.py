436203946bcbe5bbcf25639bbe67af8f6bf995d4
Efficiency300 <m9989323954@gmail.com>
2025-01-08 02:47:26 +0500

    first commit

---
+++ services/llm_service.py
@@ -0,0 +1,49 @@
+import json
+from asyncio import Lock
+from openai import AsyncOpenAI
+from config.config import Config
+from pathlib import Path
+from services.promt import promt
+from utils.JsonDataBase import JSONDatabase
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+talk_id_json = f"{BASE_DIR}/config/thread_id.json"
+file_lock = Lock()
+
+db = JSONDatabase(talk_id_json)
+FULL_PROMPT = promt
+async def thread(message_text: str, chat_id: str) -> list[str | dict]:
+
+    try:
+        async with file_lock:
+            client = AsyncOpenAI(api_key=Config.OPENAI_API_KEY)
+            thread_id = await db.get(chat_id) if await db.exists(chat_id) else None
+            if not thread_id:
+                thread = await client.beta.threads.create()
+                thread_id = thread.id
+                await db.add(chat_id, thread_id)
+
+            await client.beta.threads.messages.create(
+                thread_id=thread_id,
+                role="user",
+                content=message_text
+            )
+
+            run = await client.beta.threads.runs.create_and_poll(
+                thread_id=thread_id,
+                model="gpt-4o",
+                assistant_id=Config.ASSIST_ID,
+                temperature=0.5,
+                instructions=FULL_PROMPT
+            )
+
+            if run.status == "completed":
+                messages = await client.beta.threads.messages.list(thread_id=thread_id)
+                messages_json = json.loads(messages.model_dump_json())
+                response = messages_json["data"][0]["content"][0]["text"]["value"]
+                return [response, messages_json]
+
+            return ["Model run not completed"]
+
+    except Exception as e:
+        return [str(e)]
---
+++ services/stt_service.py
@@ -0,0 +1,27 @@
+import aiohttp
+from openai import AsyncOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class STTService:
+    def __init__(self):
+        self.client = AsyncOpenAI()
+
+    async def transcribe(self, url, model="whisper-1"):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    audio_content = await response.read()
+                    try:
+                        transcript = await self.client.audio.transcriptions.create(
+                            model=model,
+                            file=("audio_file.m4a", audio_content)
+                        )
+                        return transcript.text
+                    except Exception as e:
+                        print(f"Ошибка транскрибирования: {e}")
+                        return None
+                else:
+                    print("Ошибка при скачивании аудиофайла")
+                    return None
